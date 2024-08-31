5296698210a74c2b9148c613cd3f2df29ee64f13
samww <jordansanchez100603@gmail.com>
2024-08-31 13:00:12 -0400

    Renames, created edit contact might be working??

--- LAMPAPI/EditContact.php
+++ LAMPAPI/EditContact.php
@@ -1,20 +1,21 @@
 <?php
 	$inData = getRequestInfo();
-	
+
+	$contactId = $inData["contactId"];
 	$name = $inData["name"];
 	$userId = $inData["userId"];
 	$email = $inData["email"]
 	$phone = $inData["phone"]
 
 	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331"); //TODO
-	if ($conn->connect_error) 
+	if ($conn->connect_error)
 	{
 		returnWithError( $conn->connect_error );
-	} 
+	}
 	else
 	{
-		$stmt = $conn->prepare("INSERT into Contacts (UserId,ContactName,Email,Phone) VALUES(?,?,?,?)"); //TODO date created can possibly be automatically created through database?
-		$stmt->bind_param("ssss", $userId, $name, $email, $phone);
+		$stmt = $conn->prepare("UPDATE Contacts SET ContactName = ?, Email = ?, Phone = ? WHERE id = ? AND UserId = ?"); //TODO test
+		$stmt->bind_param("sssii", $name, $email, $phone, $contactId, $userId);
 		$stmt->execute();
 		$stmt->close();
 		$conn->close();
@@ -31,11 +32,11 @@
 		header('Content-type: application/json');
 		echo $obj;
 	}
-	
+
 	function returnWithError( $err )
 	{
 		$retValue = '{"error":"' . $err . '"}';
 		sendResultInfoAsJson( $retValue );
 	}
-	
+
 ?>
