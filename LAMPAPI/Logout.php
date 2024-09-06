693af7df55c2587ce4d6c3d62022ee4ac245b793
dylanjgreg <dylan.greg2003@gmail.com>
2024-09-06 19:21:02 -0400

    Update Logout.php

--- LAMPAPI/Logout.php
+++ LAMPAPI/Logout.php
@@ -2,19 +2,27 @@
 session_start();
 
 // Unset all session variables
-$_SESSION = array();
+$_SESSION = [];
 
-// If it's desired to kill the session entirely, delete the session cookie
+// Delete the session cookie
 if (ini_get("session.use_cookies")) {
     $params = session_get_cookie_params();
-    setcookie(session_name(), '', time() - 42000,
-        $params["path"], $params["domain"],
-        $params["secure"], $params["httponly"]
+    setcookie(
+        session_name(), 
+        '', 
+        time() - 42000, 
+        $params["path"], 
+        $params["domain"], 
+        $params["secure"], 
+        $params["httponly"]
     );
 }
 
 // Destroy the session
 session_destroy();
+
+// Clear the $_SESSION superglobal to ensure it is completely empty
+$_SESSION = [];
 
 // Redirect to login page or homepage
 header("Location: index.php");
