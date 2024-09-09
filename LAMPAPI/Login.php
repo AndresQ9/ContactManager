8389215a2e8c06231aff6d69b8658a4c8594bd71
samww <jordansanchez100603@gmail.com>
2024-09-09 00:48:30 -0400

    debug

--- LAMPAPI/Login.php
+++ LAMPAPI/Login.php
@@ -10,7 +10,7 @@
 	$lastName = "";
 
 	$conn = new mysqli("localhost", "root", ":dQD:QR4/HMX", "contactmanager"); //need to change user and password when deployed
-	if( $conn->connect_error )
+	if( true)//$conn->connect_error )
 	{
 		returnWithError( $conn->connect_error );
 	}
