2c8244ad0dfc7698b29455f20bd366728634741c
samww <jordansanchez100603@gmail.com>
2024-09-12 15:06:35 -0400

    test loading

--- js/home.js
+++ js/home.js
@@ -160,7 +160,7 @@ function loadContacts() {
         })// Parse the JSON response from the server
         .then(json => {
             console.log(json);
-            contacts = json;
+            contacts = json.contacts;
             if (json.error === "") {
                 window.location.href = 'login.html';  // Redirect to your home page
             } else {
