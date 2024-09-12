c0079b7bbf17231623ada1052398e9bf9ee74a58
samww <jordansanchez100603@gmail.com>
2024-09-12 15:10:42 -0400

    test loading

--- LAMPAPI/loadContacts.php
+++ LAMPAPI/loadContacts.php
@@ -4,7 +4,7 @@
 	$page = intval($inData["page"]);
 	$offset = 10*($page - 1);
 	$searchCount = 0;
-	$searchResults = "{contacts: [";
+	$searchResults = "{ contacts: [";
 
 	$conn = new mysqli("localhost", "root", ":dQD:QR4/HMX", "contactmanager"); //need to change user and password when deployed
 	if( $conn->connect_error ){
@@ -37,9 +37,9 @@
             				$searchResults .= ",";
             			}
             			$searchCount++;
-            			$searchResults .= '{"firstName:' . $row["firstName"] . '", lastName:"' . $row["lastName"] . '", phone: "' . $row["phone"] . '", email: "' . $row["email"] . '"}' ;
+            			$searchResults .= '{ "firstName: ' . $row["firstName"] . '", lastName: "' . $row["lastName"] . '", phone: "' . $row["phone"] . '", email: "' . $row["email"] . '"}' ;
             		}
-                $searchResults .= '], error: ""}';
+                $searchResults .= '], error: "" }';
 
             if( $searchCount == 0 )
                     {
