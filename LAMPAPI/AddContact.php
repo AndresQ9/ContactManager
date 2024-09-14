06d1da1cefda15e54a2d555ec4c1e3e143fae120
dylanjgreg <dylan.greg2003@gmail.com>
2024-09-14 12:08:40 -0400

    Update AddContact.php

--- LAMPAPI/AddContact.php
+++ LAMPAPI/AddContact.php
@@ -1,19 +1,19 @@
 <?php
-	$inData = getRequestInfo();
-	
+	$inData = json_decode(file_get_contents('php://input'), true);
+
 	$name = $inData["name"];
 	$userId = $inData["userId"];
 	$email = $inData["email"];
 	$phone = $inData["phone"];
 
-	$conn = new mysqli("localhost", "root", ":dQD:QR4/HMX", "contactmanager"); //need to change user and password when deployed
-	if ($conn->connect_error) 
+	$conn = new mysqli("localhost", "root", "dylanswebsite", "contactmanager"); //need to change user and password when deployed
+	if ($conn->connect_error)
 	{
 		returnWithError( $conn->connect_error );
-	} 
+	}
 	else
 	{
-		$stmt = $conn->prepare("INSERT into Contacts (UserId,ContactName,Email,Phone) VALUES(?,?,?,?)"); //TODO date created can possibly be automatically created through database?
+		$stmt = $conn->prepare("INSERT into Contacts (UserId,ContactName,Email,Phone) VALUES(?,?,?,?)"); //TODO test
 		$stmt->bind_param("ssss", $userId, $name, $email, $phone);
 		$stmt->execute();
 		$stmt->close();
@@ -21,21 +21,11 @@
 		returnWithError("");
 	}
 
-	function getRequestInfo()
-	{
-		return json_decode(file_get_contents('php://input'), true);
-	}
-
-	function sendResultInfoAsJson( $obj )
-	{
-		header('Content-type: application/json');
-		echo $obj;
-	}
-	
 	function returnWithError( $err )
 	{
 		$retValue = '{"error":"' . $err . '"}';
-		sendResultInfoAsJson( $retValue );
+		header('Content-type: application/json');
+		echo $retValue;
 	}
-	
+
 ?>
