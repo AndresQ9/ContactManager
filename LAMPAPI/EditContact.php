896eca6bb7a14c4b804d748ff8c3b47a3037d72c
samww <jordansanchez100603@gmail.com>
2024-09-14 18:50:59 -0400

    test loading

--- LAMPAPI/EditContact.php
+++ LAMPAPI/EditContact.php
@@ -6,10 +6,10 @@ error_reporting(E_ALL);
 
 	$inData = getRequestInfo();
 
-	$contactId = $inData["contactId"];
+	$contactId = intval($inData["contactId"]);
 	$firstName = $inData["firstName"];
 	$lastName = $inData["lastName"]
-	$userId = $inData["userId"];
+	$userId = intval($inData["userId"]);
 	$email = $inData["email"];
 	$phone = $inData["phone"];
 
@@ -22,7 +22,6 @@ error_reporting(E_ALL);
 	{
 		$stmt = $conn->prepare("UPDATE Contacts SET firstName = ?, lastName = ? , Email = ?, Phone = ? WHERE ContactID = ? AND userId = ?"); //TODO test
 		$stmt->bind_param("ssssii", $firstName, $lastName, $email, $phone, $contactId, $userId);
-		echo $stmt;
 		$stmt->execute();
 		$stmt->close();
 		$conn->close();
