d69423941a20db122731126cd746fc968731b75f
samww <jordansanchez100603@gmail.com>
2024-09-15 11:58:37 -0400

    test loading

--- LAMPAPI/loadContacts.php
+++ LAMPAPI/loadContacts.php
@@ -17,7 +17,7 @@
 	else{
                             //if there's a search
         if( $inData["search"] != ""){
-            $stmt = $conn->prepare("SELECT * FROM contacts WHERE userId = ? AND (firstname LIKE ? OR lastname LIKE ? OR email LIKE ? OR phone LIKE ?)");
+            $stmt = $conn->prepare("SELECT * FROM contacts WHERE userId = ? AND (firstname LIKE ? OR lastname LIKE ? OR email LIKE ? OR phone LIKE ?) Limit ? OFFSET ?");
             $stmt->bind_param("issss", $userId, $search, $search, $search, $search);
             $stmt->execute();
             $result = $stmt->get_result();
