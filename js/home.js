af13c6845ae904c1de51ebebb2c7643a32fae14d
samww <jordansanchez100603@gmail.com>
2024-09-15 10:30:00 -0400

    test loading

--- js/home.js
+++ js/home.js
@@ -131,7 +131,6 @@ function openEditModal(contact) {
 
 // Function to submit the contact from the modal form
 function submitContact() {
-    closeModal();
     const contactData = {
         contactId: editingContactId,
         firstName: document.getElementById('name').value,
@@ -140,6 +139,7 @@ function submitContact() {
         email: document.getElementById('email').value,
         userId: userId // Include the userId in the contact data
     };
+    closeModal();
     if((document.getElementById('modalTitle').textContent === 'Create a New Contact')) {
         fetch('http://www.jordanshouse.site/ContactManager/LAMPAPI/saveContact.php', {
             method: 'POST',
