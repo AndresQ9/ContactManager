0359e6d5ab74ccefc4d0083ce370f84d8d832f4e
samww <jordansanchez100603@gmail.com>
2024-09-24 11:44:55 -0400

    Ahinya's changes

--- js/home.js
+++ js/home.js
@@ -167,7 +167,6 @@ function submitContact(id) {
 
         if (contactData.firstName === '') {
             name.style.borderColor = 'red';
-            console.log("hi");
             name.style.boxShadow = '0px 0px 5px red';
         } else {
             name.style.borderColor = origColor;
